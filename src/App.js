096221155a2d679313746f59c9b24e3703cf4fb7
Luca Lamanna <luca.lamanna@hotmail.com>
2020-01-25 15:31:33 -0500

    added header component and links to home and shop page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 import Homepage from './pages/Homepage';
 import Shop from './pages/Shop';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={Shop}/>
