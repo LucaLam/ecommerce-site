bd352073e6849f524410cf4cc27e3dbdd795e0b3
Luca Lamanna <luca.lamanna@hotmail.com>
2020-02-10 14:53:48 -0500

    moved directory and shop data into own reducer and created respective selectors and updated components with new redux flow directory/shop

--- src/components/collections-overview/CollectionsOverview.js
+++ src/components/collections-overview/CollectionsOverview.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 
 import PreviewCollection from '../PreviewCollection';
-import {selectCollections} from '../../Redux/shop/shop.selector';
+import {selectCollectionsForPreview} from '../../Redux/shop/shop.selector';
 
 import './collectionsoverview.scss';
 
@@ -18,7 +18,7 @@ const CollectionsOverview = ({collections}) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -2,18 +2,27 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 
-// import CollectionItem from '../../components/CollectionItem'
+import CollectionItem from '../../components/CollectionItem'
 
 import { selectCollection} from '../../Redux/shop/shop.selector'
 
 import './collection.scss';
 
 const Collection = ({ collection }) => {
-    console.log(collection);
+    const {title, items} = collection;
     return(
+    <>
     <div className='collection'>
-        <h2> Category Page</h2>
+        <h2 className='title'>{title}</h2>
     </div>
+    <div className='items'>
+        {
+            items.map(item => (
+                <CollectionItem key={item.id} item={item} />
+            ))
+        }
+    </div>
+    </>
 );
 };
 
